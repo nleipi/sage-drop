ea80d2925ff92b9b95916c085171b31c65505b66
Nikolay Leipi <nikolay.semykin@googlemail.com>
2025-11-21 15:49:23 +0100

    Migrate to click

--- src/sage_drop/__init__.py
+++ src/sage_drop/__init__.py
@@ -1,9 +1,16 @@
 import os
 import argparse
+import click
 from datetime import datetime
 
 from sage_drop.client import Client
 from sage_drop import cli as commands
+
+
+locations = {
+    'office': 0,
+    'home': 1
+}
 
 
 def valid_date(s):
@@ -14,46 +21,55 @@ def valid_date(s):
                 'Invalid date format. Please use YYYY-MM-DD.')
 
 
-def cli():
+def get_client():
     try:
         user_name = os.environ['SAGE_USER']
         password = os.environ['SAGE_PASSWORD']
     except Exception:
         print('Missing environment variables SAGE_USER and SAGE_PASSWORD')
-        return
 
-    parser = argparse.ArgumentParser(
-            prog='sage',
-            description='Because sage sucks'
-            )
-    subparsers = parser.add_subparsers(
-            dest='command',
-            title='commands',
-            )
-    subparsers.add_parser('come', help='Clock in')
-    subparsers.add_parser('go', help='Clock out')
-    subparsers.add_parser('break', help='Clock out for a break')
-    subparsers.add_parser('balance', help='Show current balance')
-    times_parser = subparsers.add_parser(
-            'times',
-            help='Show times'
-            )
-    times_parser.add_argument('from', nargs='?', type=valid_date,
-                              help='From date')
-    times_parser.add_argument('to', nargs='?', type=valid_date,
-                              help='To date')
-    args = parser.parse_args()
-    if args.command is None:
-        parser.print_help()
-        return
-    client = Client('http://sage.fabfab.de/', user_name, password)
-    if args.command == 'balance':
-        commands.balance(client)
-    elif args.command == 'come':
-        commands.come(client)
-    elif args.command == 'go':
-        commands.go(client, False)
-    elif args.command == 'break':
-        commands.go(client, True)
-    elif args.command == 'times':
-        commands.times(client, getattr(args, 'from'), args.to)
+    return Client('http://sage.fabfab.de/', user_name, password)
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command(help="Show current balance")
+def balance():
+    commands.balance(get_client())
+
+
+@cli.command(help="Clock in")
+def come():
+    commands.come(get_client(), False)
+
+
+@cli.command(help="Clock out")
+def go():
+    commands.go(get_client(), False)
+
+
+@cli.command('break', help="Clock out for a break")
+def go_break():
+    commands.go(get_client(), True)
+
+
+@cli.command('return', help="Clock in")
+def come_back():
+    commands.come(get_client(), True)
+
+
+@cli.command(help="List times")
+@click.option("-f", "--from", "from_date",
+              type=valid_date,
+              help="Times from this date, or monday if empty")
+@click.option("-t", "--to", "to_date",
+              type=valid_date,
+              help="Times to this date, or now if empty")
+@click.option("-r", "--range", "range_days",
+              type=int,
+              help="Last X days")
+def times(from_date, to_date, range_days):
+    commands.times(get_client(), from_date, to_date, range_days)
